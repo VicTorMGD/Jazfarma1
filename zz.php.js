7e40c8983c01f3f7ef46f2fab6831dad380bb611
VicTorM <vmiguelg@gmail.com>
2025-11-25 00:58:26 -0500

    primer avance, copiado del sistema a git

---
+++ zz.php.js
@@ -0,0 +1,29 @@
+function buscar_usuario(dato){
+        funcion = 'buscar_usuario';
+        $.post('../controlador/UsuarioController.php',{dato,funcion},(response)=>{
+            console.log(response);
+            let nombre ='';
+            let apellidos ='';
+            let edad ='';
+            let dni ='';
+            let tipo ='';
+            let telefono ='';
+            let residencia ='';
+            let correo ='';
+            let sexo ='';
+            let adicional ='';
+            const usuario = JSON.parse(response);
+            nombre+=`${usuario.nombre}`;
+            apellidos+=`${usuario.apellidos}`;
+            edad+=`${usuario.edad}`;
+            dni+=`${usuario.dni}`;
+            tipo+=`${usuario.tipo}`;
+            telefono+=`${usuario.telefono}`;
+            residencia+=`${usuario.residencia}`;
+            correo+=`${usuario.correo}`;
+            sexo+=`${usuario.sexo}`;
+            adicional+=`${usuario.adicional}`;
+            $('#nombre_us').html(nombre);
+        })
+
+    }
